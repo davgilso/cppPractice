9e369b49e2709610ffedfd40911fa83a59954ab7
David Gilson <david.gilson@youview.com>
2018-04-02 09:39:59 +0100

    Tidy project layout

--- vectors/cmake_install.cmake
+++ vectors/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/david/projects/projVectors
+# Install script for directory: /home/david/projects/cppPractice/vectors
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/david/projects/projVectors/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/david/projects/cppPractice/vectors/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
