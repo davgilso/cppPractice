940aec0a37d2ce9e9d33456441a76cc7aca44f22
David Gilson <david.gilson@youview.com>
2018-04-07 22:31:36 +0100

    Added some shared pointer stuff to test ref_count.

--- smartPointers/src/main.cpp
+++ smartPointers/src/main.cpp
@@ -4,19 +4,67 @@
 class logger
 {
 	private:
-		virtual std::string print(std::string message) = 0;
+		virtual void print(std::string message) = 0;
 		
 	
 };
 
+class apple : public logger {
+	
+	std::string name;
+	
+public:
+	
+	apple(void):name("apple"){
+	}
+	
+	void print(std::string message)
+	{
+		std::cout << "Apple:" << message << std::endl;
+	}
+	
+};
 
+class orange : public logger {
+	
+	std::string name;
+
+public:
+	
+	orange (): name("orange"){
+	}
+	
+	void print(std::string message)
+	{
+		std::cout << "orange: " << message << std::endl;
+	}
+	
+	void notAnOrange(std::shared_ptr<apple> a)
+	{
+		a->print("Coming from orange");
+		std::cout << "Use count:" << a.use_count() << std::endl;
+	}
+	
+};
 
 int main(int argc, char* argv[])
 {
 	// This little prog will have two obj that implement logger,
 	// then a function to print all logs will be called, that
 	// takes a shared pointer as an arg. The pointer will be passed
-	// by reference so to force a COPY and increase ref count. I will also
+	// by reference so to force an increase of ref count. I will also
 	// print out the ref count.
+	
+	std::shared_ptr<apple> a1 = std::make_shared<apple>();
+	std::shared_ptr<orange> o1 = std::make_shared<orange>();
+	
+	/* This will increase the reference count.*/
+	o1->notAnOrange(a1);
+	
+	/* This will not increase the count, but a1 will now be invalid.*/
+	o1->notAnOrange(std::move(a1));
+	
+	std::cin.get();
 	return 0;
 }
+
