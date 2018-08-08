a6fb687cf45f60b4cb23a491fd7e5c53eb911849
david <gilsonstourbridge@gmail.com>
2018-08-08 23:26:38 +0100

    update

--- modernCppChallenge/maths/problem7_AmicableNumbers/amicableNumbers.cpp
+++ modernCppChallenge/maths/problem7_AmicableNumbers/amicableNumbers.cpp
@@ -6,43 +6,49 @@
 using namespace std;
 
 bool isAmicable(int input1, int input2, std::vector<int>*amicableList);
-int getRealNumberTotal(int input);
+int getRealNumberTotal(int input, int range);
 
 
 int main()
 {
-    int range = 100000;
+    int range = 1000000;
     std::vector<int>amicableList;
 
     for (int i =4; i < range; i++)
     {
         int tmp1, tmp2 = 0;
 
-        tmp1 = getRealNumberTotal(i);
-        tmp2 = getRealNumberTotal(tmp1);
+        tmp1 = getRealNumberTotal(i, range);
+        tmp2 = getRealNumberTotal(tmp1, range);
 
-        if ((isAmicable(i, tmp2, &amicableList)) && (tmp1 != i))
+        if ((tmp1 >= range) || (tmp2 >= range))
+        {
+            break;
+        }
+        else if ((isAmicable(i, tmp2, &amicableList)) && (tmp1 != i))
         {
             amicableList.push_back(tmp1);
             amicableList.push_back(tmp2);
             cout << tmp2 << "..." << tmp1 << "\n";
         } 
+        else{}
     }
 }
 
-int getRealNumberTotal(int input)
+int getRealNumberTotal(int input, int range)
 {
     int total = 0;
 
-
-    for (int i =1; i < input; i++)
+    if (input < range)
     {
-        if ((input % i ) == 0)
+        for (int i =1; (i < input); i++)
         {
-            total += i;
-        }   
+            if ((input % i ) == 0)
+            {
+                total += i;
+            }   
+        }
     }
-
     return total;
 }
 
